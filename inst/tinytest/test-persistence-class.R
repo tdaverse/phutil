8166fe4fbe9fc6ad69c67fdf69d2472e0616147d
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2025-04-02 15:10:11 +0200

    Improve coverage.

--- inst/tinytest/test-persistence-class.R
+++ inst/tinytest/test-persistence-class.R
@@ -2,6 +2,34 @@ using("tinysnapshot")
 
 opts <- options(cli.width = 80)
 
+expect_error(as_persistence(list()))
+
+M <- as.matrix(noisy_circle_ripserr)
+
+mat <- as_persistence(M)
+expect_equal(mat$metadata$data, "?")
+expect_equal(mat$metadata$engine, "?")
+expect_equal(mat$metadata$simplicial_complex, "?")
+expect_equal(mat$metadata$call, "?")
+expect_equal(mat$metadata$parameters, list())
+
+params <- list(maxdimension = 1L, maxscale = 1.6322)
+mat <- as_persistence(M, parameters = params)
+expect_equal(mat$metadata$parameters, params)
+expect_snapshot_print(mat, label = "print-mat-persistence")
+
+wrong_df <- noisy_circle_ripserr
+wrong_df$morevar <- 1L
+expect_error(as_persistence(wrong_df))
+
+wrong_df <- noisy_circle_ripserr
+wrong_df$dimension <- NULL
+expect_error(as_persistence(wrong_df))
+
+wrong_df <- noisy_circle_ripserr
+colnames(wrong_df) <- c("x", "y", "z")
+expect_error(as_persistence(wrong_df))
+
 ripserr <- as_persistence(noisy_circle_ripserr)
 expect_equal(sapply(ripserr$pairs, ncol), c(2L, 2L))
 expect_equal(as.character(ripserr$metadata$data), "?")
@@ -24,6 +52,20 @@ expect_equal(tda_rips$metadata$parameters$maxscale, 1.6322)
 expect_snapshot_print(as_persistence(tda_rips), label = "print-tda-rips-persistence")
 
 expect_equal(ncol(get_pairs(tda_rips, dimension = 1)), 2L)
+expect_error(get_pairs(M, dimension = 1))
+expect_equal(nrow(get_pairs(tda_rips, dimension = 2)), 0L)
+expect_equal(ncol(get_pairs(tda_rips, dimension = 2)), 2L)
+
 expect_equal(ncol(as.data.frame(tda_rips)), 3L)
+row_names <- c(paste("0", 1:100, sep = "-"), paste("1", 1:2, sep = "-"))
+expect_equal(
+  rownames(as.data.frame(tda_rips, optional = FALSE)),
+  row_names
+)
+row_names <- paste0("row", 1:102)
+expect_equal(
+  rownames(as.data.frame(tda_rips, row.names = row_names)),
+  row_names
+)
 
 options(opts)
