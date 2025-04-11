39306d4f31cef26ee91686581a254aa4ec39b35a
Jason Cory Brunson <cornelioid@gmail.com>
2025-04-11 09:33:33 -0400

    initialize details to distance documentation

--- R/distances.R
+++ R/distances.R
@@ -24,6 +24,26 @@
 #'   to compute the distance. Defaults to `0L`. This is only used if `x` and `y`
 #'   are objects of class [persistence].
 #'
+
+#' @details A matching \eqn{\varphi : D_1 \to D_2} between persistence diagrams
+#'   is a bijection of multisets, where both diagrams are assumed to have all
+#'   points on the diagonal with infinite multiplicity. The
+#'   _\eqn{p}-Wasserstein_ distance between \eqn{D_1} and \eqn{D_2} is defined
+#'   as the infimum over all matchings of the expression
+#'
+#'   \deqn{W_p(D_1,D_2) = \left( \sum_{x \in D_1}{\lVert x - \varphi(x)
+#'   \rVert^p} \right)^{\frac{1}{p}}}
+#'
+#'   that can be thought of as the Minkowski distance between the diagrams
+#'   viewed as vectors on the shared coordinates defined by the matching
+#'   \eqn{\varphi}. The norm \eqn{\lVert \cdot \rVert} can be arbitrary; as
+#'   implemented here, it is the infitity norm \eqn{\lVert (x_1,x_2)
+#'   \rVert_\infty = \max(x_1,x_2)}. In the limit \eqn{p \to \infty}, the
+#'   Wasserstein distance becomes the _bottleneck distance_:
+#'
+#'   \deqn{B(D_1,D_2) = \sup_{x \in D_1}{\lVert x - \varphi(x) \rVert}}
+#'
+
 #' @returns A numeric value storing the Bottleneck distance between the two
 #'   persistence diagrams.
 #'
