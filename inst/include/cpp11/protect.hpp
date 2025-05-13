a430289b1ef50a973e5b820e1b0994e5d9525fe4
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2025-05-13 22:58:13 +0200

    Comment setcar in unwind_protect.

--- inst/include/cpp11/protect.hpp
+++ inst/include/cpp11/protect.hpp
@@ -71,10 +71,6 @@ template <typename Fun, typename = typename std::enable_if<std::is_same<
     // R_UwindProtect does a normal exit the memory shouldn't be protected, so we
     // unset it here before returning the value ourselves.
     // SETCAR(token, R_NilValue);
-    SEXP lhs = CAR(token);
-    SEXP rhs = CDR(token);
-    SETCDR(lhs, rhs);
-    SETCAR(rhs, lhs);
 
     return res;
   }
